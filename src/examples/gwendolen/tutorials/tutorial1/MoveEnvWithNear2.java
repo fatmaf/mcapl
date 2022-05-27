de638fcdc0d5a9f47d16b229743a35702e55bdbd
fatmaf <ffaruq@gmail.com>
2022-05-27 11:47:18 +0100

    partial plan - testing with full plan such that at l2 you get the percept, need to debug this

---
+++ src/examples/gwendolen/tutorials/tutorial1/MoveEnvWithNear2.java
@@ -0,0 +1,51 @@
+package gwendolen.tutorials.tutorial1;
+
+import ail.mas.DefaultEnvironment;
+import ail.syntax.Action;
+import ail.syntax.Predicate;
+import ail.syntax.Unifier;
+import ail.util.AILexception;
+
+public class MoveEnvWithNear2 extends DefaultEnvironment {
+
+	String curloc="l1";
+
+	@Override
+	public Unifier executeAction(String agName, Action act) throws AILexception {
+		if(act.getFunctor().equals("move_to"))
+		{
+			String newlocs = act.getTerm(0).toString();
+			Predicate newloc = new Predicate("at");
+			newloc.addTerm(new Predicate(newlocs));
+			
+			Predicate oldloc = new Predicate("at"); 
+			oldloc.addTerm(new Predicate(curloc));
+			
+			removePercept(agName,oldloc); 
+			addPercept(agName,newloc);
+			curloc = newlocs;
+			
+			Predicate nearloc = new Predicate("near"); 
+			nearloc.addTerm(new Predicate("l6"));
+			//adding the near percept 
+			//when we get to l4 we realise that we are "near l6" 
+			if(newlocs.equalsIgnoreCase("l2"))
+			{
+				addPercept(agName,nearloc);
+				
+			}
+			else
+			{
+				// it might be there already and we might have to remove it 
+				// putting this in move because that is what decides it 
+				removePercept(agName,nearloc);
+			}
+			
+			
+		}
+		Unifier u =super.executeAction(agName, act);
+		return u;//super.executeAction(agName, act);
+	}
+	
+
+}
