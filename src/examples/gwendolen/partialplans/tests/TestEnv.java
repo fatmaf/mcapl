cdf686604155fef4295f19ea14cd25c172ba2c2d
fatmaf <ffaruq@gmail.com>
2022-06-13 16:52:36 +0100

    moved all my stuff out of the tutorials folder

---
+++ src/examples/gwendolen/partialplans/tests/TestEnv.java
@@ -0,0 +1,26 @@
+package gwendolen.partialplans.tests;
+
+import ail.mas.DefaultEnvironment;
+import ail.syntax.Action;
+import ail.syntax.Term;
+import ail.syntax.Literal;
+import ail.syntax.NumberTermImpl;
+import ail.syntax.Unifier;
+import ail.util.AILexception;
+
+public class TestEnv extends DefaultEnvironment {
+
+	@Override
+	public Unifier executeAction(String agName, Action act) throws AILexception {
+		if(act.getFunctor().equals("move"))
+		{
+			Term loc = act.getTerm(1);
+			Literal thing = new Literal("thing"); 
+			thing.addTerm(new NumberTermImpl("123"));
+			thing.addTerm(new NumberTermImpl("1"));
+			addPercept(thing);
+		}
+		return super.executeAction(agName, act);
+	}
+
+}
